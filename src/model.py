64abf28b20910f5dcdca9595fe3ff96113817d76
Pierre Epron <pierre.epron@gmail.com>
2024-03-06 21:58:32 +0100

    fix clm forward

--- src/model.py
+++ src/model.py
@@ -66,7 +66,6 @@ class MultiHeadCLM(nn.Module):  # TODO: Args for model params
             output_attentions=output_attentions,
             output_hidden_states=output_hidden_states,
             return_dict=return_dict,
-            label_id=label_id,
         )
         # print("outputs.hidden_states[self.hidden_states_idx]: ", outputs['hidden_states'][-1].shape)
 
