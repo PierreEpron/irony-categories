85ed08e6840892c5da1fd30533de886d190c0e5e
Pierre Epron <pierre.epron@gmail.com>
2024-04-16 15:51:11 +0200

    add postpro

--- src/postprocessing.py
+++ src/postprocessing.py
@@ -8,3 +8,25 @@ def is_valid_enum(text, eos_token='</s>'):
     if not text.endswith(eos_token):
         return False
     return True
+
+def parse_enum(text):
+    previous = None
+    outputs = []
+    for m in head_pattern.finditer(text):
+        if previous:
+            outputs.append({
+                'header': previous.group(1).strip(),
+                'argument': text[previous.end():m.start()].strip()
+            })
+        previous = m
+    return outputs
+
+
+def parse_enums(examples):
+    outputs = []
+
+    for example in examples:
+        label = int(example['label_id'] == example['pred'])
+        outputs.extend([{**x, 'label':label} for x in parse_enum(example['pred_expls']['enum_exp'])])
+
+    return outputs
