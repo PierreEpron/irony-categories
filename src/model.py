3341933d15abd57a8bf6ad2c30f42c58328db089
Pierre Epron <pierre.epron@gmail.com>
2024-04-26 13:55:46 +0200

    add prediction loop

--- src/model.py
+++ src/model.py
@@ -3,10 +3,10 @@ from typing import Optional
 from pathlib import Path
 import json
 
+from pytorch_lightning.loggers import TensorBoardLogger, CSVLogger
 from transformers import AutoModel, BitsAndBytesConfig
 from peft import LoraConfig, get_peft_model, PeftModel
 from sklearn.metrics import matthews_corrcoef
-
 import lightning as L
 import torch
 
@@ -33,7 +33,7 @@ class PeftConfig:
 
     lora_r: Optional[int] = field(default=64, metadata={"help": "the r parameter of the LoRA adapter"})
     lora_alpha: Optional[int] = field(default=16, metadata={"help": "the alpha parameter of the LoRA adapter"})
-    target_modules: Optional[list] = field(default_factory=lambda:["q_proj", "v_proj"], metadata={"help": "layers to plug LoRA adapter."})
+    target_modules: Optional[str] = field(default="q_proj,v_proj", metadata={"help": "layers to plug LoRA adapter. Splitted by comma."})
     lora_dropout: Optional[float] = field(default=0.1, metadata={"help":"use or not dropout for the LoRA adapter"})
     lora_bias: Optional[str] = field(default="none", metadata={"help":"use or not bias for the LoRA adapter"})
     inference_mode: Optional[bool] = field(default=False, metadata={"help": "Is the adapter used for inference or not"})
@@ -52,6 +52,7 @@ class TrainingConfig:
     train_batch_size: Optional[int] = field(default=4, metadata={"help":"Size of a train batch"})
     val_batch_size: Optional[int] = field(default=4, metadata={"help":"Size of a validation batch"})
     max_len: Optional[int] = field(default=105, metadata={"help":"Maximum length of a tokenized example. If greater than this length, drop the example."})
+
 
 class FFClassifer(torch.nn.Module):
 
@@ -149,7 +150,7 @@ class LLMClassifier(L.LightningModule):
             self.llm_model = get_peft_model(self.llm_model, LoraConfig(
                 r=self.peft_config.lora_alpha,
                 lora_alpha=self.peft_config.lora_r,
-                target_modules=self.peft_config.target_modules,
+                target_modules=self.peft_config.target_modules.split(','),
                 lora_dropout=self.peft_config.lora_dropout,
                 bias=self.peft_config.lora_bias,
                 inference_mode=self.peft_config.inference_mode
@@ -193,7 +194,8 @@ class LLMClassifier(L.LightningModule):
         # TODO: Should be optional 
         training_config = TrainingConfig(**json.loads((path / "training_config.json").read_text(encoding='utf-8')))  
         return cls(llm_config, clf_model_name=path, peft_model_name=path, training_config=training_config)
-    
+
+
     def forward(self, batch, batch_idx):
         outputs = self.llm_model.forward(
             input_ids=batch['input_ids'],
@@ -203,7 +205,6 @@ class LLMClassifier(L.LightningModule):
         )
         logits = self.clf_model(outputs)
         return logits
-
 
     def training_step(self, batch, batch_idx):
 
@@ -237,7 +238,6 @@ class LLMClassifier(L.LightningModule):
         self.val_targets.extend(batch['labels'].argmax(dim=-1).cpu())
 
     def on_validation_epoch_end(self):
-        print('LLMClassifier.on_validation_epoch_end')
 
         mcc = matthews_corrcoef(self.val_outputs, self.val_targets)
 
@@ -246,9 +246,58 @@ class LLMClassifier(L.LightningModule):
         self.val_targets.clear()
 
 
+    def predict_step(self, batch, batch_idx):
+        logits = self.forward(batch, batch_idx)
+        scores = torch.nn.functional.softmax(logits, dim=-1)
+        pred = scores.argmax(dim=-1)
+
+        predictions = {}
+
+        for example_id, label_id, text, scores, pred in zip(
+            batch['example_id'],
+            batch['label_id'],
+            batch['text'],
+            scores.cpu().tolist(),
+            pred.cpu().item()
+        ):
+            predictions.append({
+                'example_id':example_id,
+                'label_id':label_id,
+                'text':text,
+                'scores':scores,
+                'pred':pred
+            })
+
     def configure_optimizers(self):
         optimizer = torch.optim.AdamW([
             {"params":self.llm_model.parameters(), "lr":self.training_config.lr_peft},
             {"params":self.clf_model.parameters(), "lr":self.training_config.lr_clf}
         ])
         return optimizer
+
+
+def get_plt_loggers(path):
+    '''
+        Shortcut to get plt loggers
+    '''
+    path = Path(path) if isinstance(path, str) else path
+    if not path.is_dir():
+        path.mkdir()
+
+    tb_logger = TensorBoardLogger(path / "tb_logs", name=path.stem)
+    csv_logger = CSVLogger(path / "cv_logs", name=path.stem)
+    return [csv_logger, tb_logger]
+
+
+class CkptCallback(L.Callback):
+    def __init__(self, ckpt_path, metric_name="val_mcc", best_model_metric=-2):
+        self.ckpt_path = ckpt_path
+        self.metric_name = metric_name
+        self.best_model_metric = best_model_metric
+
+    # TODO: I used training epoch to be sure that this call is made after validation_epoch_end of pl_module
+    def on_train_epoch_end(self, trainer, pl_module):
+        current_model_metric = trainer.logged_metrics[self.metric_name]
+        if self.best_model_metric < current_model_metric:
+            self.best_model_metric = current_model_metric
+            pl_module.save(self.ckpt_path)
---
+++ src/train_llm_clf.py
@@ -0,0 +1,82 @@
+from pathlib import Path
+
+from lightning.pytorch.callbacks import LearningRateMonitor
+from transformers import (
+    HfArgumentParser,
+    AutoTokenizer
+)
+import lightning as L
+
+from src import model as M
+from src import preprocessing as P
+
+from src.tools.split_data import get_split
+from src.utils import write_jsonl
+
+
+##### Parse args and instantiate configs #####
+
+parser = HfArgumentParser([M.PretrainedLLMConfig, M.PeftConfig, M.FFClassifierConfig, M.TrainingConfig])
+llm_config, peft_config, clf_config, training_config = parser.parse_args_into_dataclasses()
+
+
+##### Load tokenizer and model #####
+
+tokenizer = AutoTokenizer.from_pretrained(llm_config.model_name)
+tokenizer.use_default_system_prompt = False
+
+model = M.LLMClassifier(llm_config, clf_config=clf_config, peft_config=peft_config, training_config=training_config)
+
+if llm_config.eos_as_pad:
+    tokenizer.pad_token_id = tokenizer.eos_token_id
+    model.llm_model.config.pad_token_id = tokenizer.eos_token_id
+
+
+##### Load and preprocess data #####
+
+train, test = P.load_semeval_taskb(return_sets="splits", urls=False, lower=False)
+train, val = get_split(training_config.current_split, training_config.split_path, train)
+
+turns = [{"role": "user", "content": "{text}"}]
+
+train_set = P.make_dataset(train, tokenizer, turns, max_len=training_config.max_len)
+val_set = P.make_dataset(val, tokenizer, turns, max_len=training_config.max_len)
+test_set = P.make_dataset(test, tokenizer, turns, max_len=training_config.max_len)
+
+# print(f"{len(train_set)}/{len(train)}, {len(val_set)}/{len(val)}, {len(test_set)}/{len(test)}")
+
+train_loader = P.make_loader(train_set, tokenizer, training_config.train_batch_size, False, True)
+val_loader = P.make_loader(val_set, tokenizer, training_config.val_batch_size, False, False)
+test_loader = P.make_loader(test_set, tokenizer, 1, True, False)
+
+# print(next(iter(train_loader)).keys())
+# print(next(iter(test_loader)).keys())
+# print(next(iter(train_loader))['input_ids'].shape)
+
+##### Train model #####
+
+lr_monitor = LearningRateMonitor(logging_interval='step')
+ckpt_callback = M.CkptCallback(ckpt_path=training_config.result_path)
+
+trainer = L.Trainer(
+    max_epochs=training_config.max_epochs,
+    enable_checkpointing=False,
+    logger=M.get_plt_loggers(training_config.result_path),
+    callbacks=[lr_monitor, ckpt_callback]
+)
+
+trainer.fit(
+    model=model, 
+    train_dataloaders=train_loader, 
+    val_dataloaders=val_loader
+)
+
+##### Evaluate model #####
+
+model = M.LLMClassifier.load(training_config.result_path)
+predictions = trainer.predict(
+    model=model, 
+    test_loader=test_loader, 
+    return_predictions=True
+)
+write_jsonl(Path(training_config.result_path) / "predictions.jsonl", predictions)
