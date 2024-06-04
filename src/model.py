1c6d93079263cc31185d30ebec8cc0a75eb41d8c
Pierre Epron <pierre.epron@gmail.com>
2024-06-04 11:34:44 +0200

    fix

--- src/model.py
+++ src/model.py
@@ -57,7 +57,7 @@ class TrainingConfig:
 
     result_path: str = field(metadata={"help": "The path used to store results"})
     current_split: Optional[int] = field(default=-1, metadata={"help": "The cross validation split to use (between 0 and 4). -1 is used to run on all splits"})
-    split_path: Optional[str] = field(default="data/splits.jsonl", metadata={"help":"The jsonl file path containing the cross validation split indices"})
+    split_path: Optional[str] = field(default="data/sem_eval/splits.jsonl", metadata={"help":"The jsonl file path containing the cross validation split indices"})
     clf_class: Optional[str] = field(default="ff", metadata={"help":"Keyword used to recover correct classifier class: [ff, mlp]"})
     lr_peft: Optional[float] = field(default=1.5e-4, metadata={"help":"Learning rate for peft adapater on LLM"})
     lr_clf: Optional[float] = field(default=1e-3, metadata={"help":"Learning rate for classifier"})
