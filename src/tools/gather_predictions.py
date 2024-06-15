a3b555fc2bded2e83cf9946de685923ba4cdda14
Pierre Epron <pierre.epron@gmail.com>
2024-06-15 16:25:20 +0200

    adapt to training changes

--- src/tools/gather_predictions.py
+++ src/tools/gather_predictions.py
@@ -1,15 +1,35 @@
+from dataclasses import dataclass, field
 from pathlib import Path
 import shutil
 import os
 
-root_path = Path(os.environ["HOME"])
-results_path = root_path / "irony-categories/results/"
+from transformers import HfArgumentParser
+from tqdm import tqdm
 
-for path in results_path.glob("*/predictions.jsonl"):
-    dst = root_path / path.parts[-2]
-    if not dst.is_dir():
-        dst.mkdir()
-    shutil.copy(path, dst / "predictions.jsonl")
-    shutil.copy(path.with_name("trainer_state.json"), dst / "trainer_state.json")
 
+HOME_PATH = Path(os.environ["HOME"])
+
+
+@dataclass
+class ScriptArguments:
+    target_path: str = field(metadata={"help":"Gather predictions and metrics inside the target folder."})
+
+
+if __name__ == "__main__":
+
+    parser = HfArgumentParser([ScriptArguments])
+    script_args = parser.parse_args_into_dataclasses()[0]
+
+    target_path = Path(script_args.target_path)
+
+    for path in tqdm(list(target_path.glob("**/predictions.jsonl")) + list(target_path.glob("**/metrics.jsonl"))):
+        print(path)
+
+
+# for path in results_path.glob("*/predictions.jsonl"):
+#     dst = root_path / path.parts[-2]
+#     if not dst.is_dir():
+#         dst.mkdir()
+#     shutil.copy(path, dst / "predictions.jsonl")
+#     shutil.copy(path.with_name("trainer_state.json"), dst / "trainer_state.json")
 
