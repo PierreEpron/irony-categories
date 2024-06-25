831ed9a960e31074da6a0d8c65b77746fea78e8b
Pierre Epron <pierre.epron@gmail.com>
2024-06-25 23:20:13 +0200

    test

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -241,7 +241,9 @@ class DataManager:
         if path.is_file():
             examples = LOADER_MAP[path.suffix](path)
             print(examples[:5])
-            examples = [example | additional_context[example['example_id']] if example['example_id'] in additional_context else {} for example in examples]
+            for example in examples:
+                for k, v in additional_context[example['example_id']]:
+                    example[k] = v
             print(examples[:5])
             return self.clean_texts(examples, keys=['text'] + additional_context_keys)
         
